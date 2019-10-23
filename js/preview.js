a4890f5ed613d7159a1c2396e608889da815b3f9
Антон Куринной <adra1985@gmail.com>
2019-10-23 05:24:46 +0300

    divided the code into modules

---
+++ js/preview.js
@@ -0,0 +1,90 @@
+'use strict';
+
+(function () {
+
+  // функция очистки старых комментариев
+
+  var clearBigPictureComments = function () {
+    var comments = bigPictureCommentsList.children;
+    for (var i = comments.length - 1; i >= 0; i--) {
+      comments[i].parentElement.removeChild(comments[i]);
+    }
+  };
+
+// функция для записи в шаблон комментариев рандомных аватарок, имен и комментариев из массивов
+
+  var renderBigPictureComment = function (comments) {
+    var commentElement = pictureCommentTemplate.cloneNode(true);
+
+    commentElement.querySelector('.social__picture').src = 'img/avatar-' + window.data.getRandomNumber(window.data.MIN_AVATAR, window.data.MAX_AVATAR) + '.svg';
+    commentElement.querySelector('.social__picture').alt = window.data.getRandomElement(window.data.NAMES);
+    commentElement.querySelector('.social__text').textContent = comments;
+
+    return commentElement;
+  };
+
+  var renderBigPictureComments = function (comments) {
+    clearBigPictureComments();
+
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < comments.length; i++) {
+      fragment.appendChild(renderBigPictureComment(comments[i]));
+    }
+
+    bigPictureCommentsList.appendChild(fragment);
+  };
+
+  var bigPictureItem = document.querySelector('.big-picture');
+  var bigPictureCommentsList = bigPictureItem.querySelector('.social__comments');
+  var pictureCommentTemplate = bigPictureCommentsList.querySelector('.social__comment');
+
+
+// убираем из показа счетсик комментариев и показ новых комментариев по ТЗ
+
+  window.data.hideElement('.social__comment-count', '.comments-loader', 'visually-hidden');
+
+// функция которая открывает большую фотографию
+
+  var openBigPicture = function () {
+    window.data.removeHidden(bigPictureItem);
+    document.addEventListener('keydown', window.form.onPictureUploadEscPress);
+  };
+
+// функция которая закрывает большую фотографию
+
+  var closeBigPicture = function () {
+    window.data.addHidden(bigPictureItem);
+    document.removeEventListener('keydown', window.form.onPictureUploadEscPress);
+  };
+
+// в функции находим блоки и записываем в них нужные данные для вывода в полноэкранном режиме
+
+  var renderBigPictureItem = function (picture) {
+
+    bigPictureItem.querySelector('.big-picture__img').firstElementChild.src = picture.url;
+    bigPictureItem.querySelector('.likes-count').textContent = picture.likes;
+    bigPictureItem.querySelector('.comments-count').textContent = picture.comments.length;
+    bigPictureItem.querySelector('.social__caption').textContent = picture.description;
+
+    renderBigPictureComments(picture.comments);
+  };
+
+// вызываем функцию рендеринга большого фото и рандомим фотографии в заданом промежутке из объекта
+
+  renderBigPictureItem(window.picture.picturesData[window.data.getRandomNumber(0, window.data.PHOTO_COUNT)]);
+
+// закрываем большую фотографию
+
+  var buttonClose = bigPictureItem.querySelector('.big-picture__cancel');
+  buttonClose.tabIndex = 0;
+
+  buttonClose.addEventListener('click', function () {
+    closeBigPicture();
+  });
+
+  window.preview = {
+    openBigPicture,
+    closeBigPicture,
+    renderBigPictureItem
+  };
+})();
---
+++ js/validation.js
@@ -0,0 +1,78 @@
+'use strict';
+
+(function () {
+
+// Валидация хэштегов и описания для фото
+
+  var hashtagUploadFile = document.querySelector('.text__hashtags');
+  var descriptionUploadFile = document.querySelector('.text__description');
+
+  var checkSimilarHashtags = function (hashtags) {
+    for (var i = 0; i < hashtags.length; i++) {
+      var currentHashtag = hashtags[i];
+      for (var j = 0; j < hashtags.length; j++) {
+        if (currentHashtag === hashtags[j] && i !== j) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  var hashtagValidation = function () {
+    var errorMessage = '';
+    var hashtagValue = hashtagUploadFile.value.trim();
+
+    if (hashtagValue === '') {
+      hashtagUploadFile.setCustomValidity(errorMessage);
+      return;
+    }
+    var hashtags = hashtagValue.toLowerCase().split(' ');
+    hashtags.forEach(function (hashtagItem) {
+      if (hashtagItem.charAt(0) !== '#') {
+        errorMessage = 'Хэштег должен начинаться с символа #';
+      } else if (hashtagItem.indexOf('#', 1) > 1) {
+        errorMessage = 'Хэш-теги разделяются пробелами';
+      } else if (hashtagItem.charAt(0) === '#' && hashtagItem.length === 1) {
+        errorMessage = 'Хеш-тег не может состоять только из одной решётки';
+      } else if (hashtags.length > 5) {
+        errorMessage = 'Допустимое количество  хэштегов  не более 5';
+      } else if (hashtagItem.length > 20) {
+        errorMessage = 'Максимальная длина одного хэш-тега 20 символов, включая решётку';
+      } else if (checkSimilarHashtags(hashtags)) {
+        errorMessage = 'Хэштеги не должны повторяться';
+      }
+    });
+
+    hashtagUploadFile.setCustomValidity(errorMessage);
+  };
+
+// валидация описания к новой картинке
+
+  function descriptionValidity() {
+    var errorMessage = '';
+    var descriptionValue = descriptionUploadFile.value.trim();
+
+    if (descriptionValue === '') {
+      descriptionUploadFile.setCustomValidity(errorMessage);
+      return;
+    }
+    var descriptions = descriptionValue.toLowerCase().split(' ');
+    descriptions.forEach(function (descriptionItem) {
+      if (descriptionItem.length > 140) {
+        errorMessage = 'Максимальная длина комментария 140 символов';
+      }
+    });
+
+    descriptionUploadFile.setCustomValidity(errorMessage);
+  }
+
+  descriptionUploadFile.addEventListener('input', descriptionValidity);
+
+  hashtagUploadFile.addEventListener('input', hashtagValidation);
+
+  window.validation = {
+    descriptionUploadFile,
+    hashtagUploadFile
+  };
+})();
